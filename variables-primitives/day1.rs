5e8e0edee69e2df8d76423db1fabce05e2fe9c28
Ejaz Shaikh <knowtechnical@gmail.com>
2021-12-21 00:35:08 +0530

    Day 1 learning ;)

---
+++ variables-primitives/day1.rs
@@ -0,0 +1,23 @@
+
+fn main(){
+
+    //** Boolean variable declarations & assignments
+    let logical = true;
+    let cool: bool = false;
+
+    //thinking ? firt true and 2nd false ;)
+    println!("Hey Bool, we missed you: {0} and you sounds cool: {1}", logical, cool);
+
+    //** Integer 
+    let logical = 100;
+    let new_wow = 100i64;
+    
+    //thinking ? both prints 100 ;)
+    println!("Never thought century {0} it will be this much fun haaw!! {1}", logical, new_wow); 
+
+    //** Float  */
+    let funn = 50.0;
+    let float_baby: f32 = 100f32;
+    println!("Floating {0} wait I'm coming {1}", funn, float_baby); 
+
+}
