fae772d9051cb1a2b0b1230062919e0b354aa414
Ejaz Shaikh <knowtechnical@gmail.com>
2021-12-22 00:37:23 +0530

    Added tuples example

---
+++ variables-primitives/day2.rs
@@ -0,0 +1,18 @@
+fn main(){
+
+    let combine_datatypes = (1,2, true, "cool");
+    println!("Welcome to tuple world {}", combine_datatypes.0);
+    
+
+    // So simply store your tuple values into variables, just WOW!
+    let (a,b,c,d) = combine_datatypes;
+    println!("WOW! It's amazing {0} ", d);
+}
+
+
+
+
+
+
+
+
