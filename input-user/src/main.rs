55ba9d82f5336972406838009d431bbe87bbeaa2
Ejaz Shaikh <ejaz.shaikh151992@gmail.com>
2022-01-28 19:19:51 +0530

    Added for input user

---
+++ input-user/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    //we've created mutated string
+    let mut your_name = String::new();
+
+    println!("Enter your name: ");
+    
+    //read input from user
+    std::io::stdin().read_line(&mut your_name).unwrap();
+
+    println!("Welcome to Rust {} ", your_name);
+}
