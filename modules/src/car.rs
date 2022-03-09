0e0f2e694d4b258d5abc04019c6e5d6b8504ed48
Ejaz Shaikh <ejaz.shaikh151992@gmail.com>
2022-03-10 01:28:35 +0530

    Day 22: Basics of modules

---
+++ modules/src/car.rs
@@ -0,0 +1,5 @@
+
+pub fn speed_bmw(){
+    println!("Wow, you're defintely shinning!");
+}
+
---
+++ modules/src/main.rs
@@ -0,0 +1,10 @@
+mod car;
+
+fn main() {
+    car::speed_bmw();
+
+    println!("Simple isn't it!");
+}
+
+
+
